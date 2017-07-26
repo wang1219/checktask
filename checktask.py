d87f733d4e2ed42313011026bcdbfd560179d499
wangyangyang12 <wangyangyang12@meituan.com>
2017-07-26 21:26:56 +0800

    add crypto

--- checktask.py
+++ checktask.py
@@ -3,14 +3,33 @@
 import os
 import os.path
 import hashlib
-import ssl
+import random
+import string
 import urllib
 import urllib2
 import json
 import base64
+from binascii import b2a_hex
 
+import execjs
 import requests
 from openpyxl import load_workbook
+from Crypto.Cipher import AES
+from Crypto import Random
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# 禁用安全请求警告
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+# DaMaTu相关配置
+DamatuUserName = 'xiaolongchang'
+DamatuUserPassWD = '858993460'
+
+# Excel表格所在位置
+Excel_filepath = './发票测试.xlsx'
+
+# 验证码保存路径
+PictureFileSavePath = '/Users/wangyangyang/Desktop/test.bmp'
 
 
 def md5str(str):  # md5加密字符串
@@ -59,20 +78,20 @@ class DamatuApi():
             return jres['ret']
 
     # 上传验证码 参数filePath 验证码图片路径 如d:/1.jpg type是类型，查看http://wiki.dama2.com/index.php?n=ApiDoc.Pricedesc  return 是答案为成功 如果为负数 则为错误码
-    def decode(self, filePath, type):
-        f = open(filePath, 'rb')
-        fdata = f.read()
-        filedata = base64.b64encode(fdata)
-        f.close()
+    def decode(self, filedata, type):
+        # f = open(filePath, 'rb')
+        # fdata = f.read()
+        # filedata = base64.b64encode(fdata)
+        # f.close()
         data = {'appID': self.ID,
                 'user': self.username,
                 'pwd': self.getPwd(),
                 'type': type,
                 'fileDataBase64': filedata,
-                'sign': self.getSign(filePath)
-                }# self.getSign(base64.b64decode(filedata))
+                'sign': self.getSign(base64.b64decode(filedata))
+                }  # self.getSign(base64.b64decode(filedata))
         res = self.post('d2File', data)
-        res = str(res).encode('utf-8')
+        # res = str(res).encode('utf-8')
         jres = json.loads(res)
         print('jres', jres)
         if jres['ret'] == 0:
@@ -117,11 +136,13 @@ class DamatuApi():
 
 
 class Bill(object):
-    def __init__(self, bills):
+    def __init__(self, bills, row=None):
         self.bill_id = ''
         self.bill_num = ''
         self.bill_date = ''
         self.bill_money = ''
+        self.verification_code = None
+        self.row = row
 
         if len(bills) == 4:
             self.bill_id = bills[0]
@@ -129,19 +150,91 @@ class Bill(object):
             self.bill_date = bills[2]
             self.bill_money = bills[3]
 
-    def _get_two_dimension_picture(self):
+    def save(self, text):
+        with open(PictureFileSavePath, 'w') as f:
+            f.write(base64.b64decode(text))
+            print(u'存储验证码')
+
+    def _get_verification_picture(self):
         try:
-            callback = "jQuery1102037866965332068503_1500810751838"
+            print('查询验证码')
+            callback = "jQuery%s_%s" % (
+                ''.join([random.choice(string.digits) for _ in range(22)]),
+                ''.join([random.choice(string.digits) for _ in range(13)]))
+            r = '0.%s' % ''.join([random.choice(string.digits) for _ in range(16)])
             fpdm = self.bill_id
-            ssl._create_default_https_context = ssl._create_unverified_context
             resp = requests.get('https://fpcyweb.tax.sh.gov.cn:1001/WebQuery/yzmQuery?callback=%s'
-                                '&fpdm=%s&r=0.8492015565279871'
-                                % (callback, fpdm), verify=False)
+                                '&fpdm=%s&r=%s' % (callback, fpdm, r), verify=False)
         except Exception as e:
             print(u'获取验证码图片错误 - %s' % str(e))
         else:
-            return json.loads(resp.content[len(callback) + 1:-1])
-        return None
+            return json.loads(resp.content[len(callback) + 1:-1]), callback
+        return None, ''
+
+    def check_task(self):
+        keys, callback = self._get_verification_picture()
+        if keys:
+            print(keys, callback)
+            print('key4:%s' % keys.get('key4'))
+
+        if keys and keys.get('key4') == '00':
+            # keys['key4'] == '00' 为最正常验证码，其他奇葩的他们解析不了
+            self.save(keys['key1'])
+
+            verification_code = dmt.decode(keys['key1'], 200)
+            print("verification_code", verification_code)
+            try:
+                verification_code = int(verification_code)
+                return False
+            except ValueError:
+                if verification_code == 'ERROR':
+                    return False
+                self.verification_code = verification_code
+                self._post_request(callback, keys.get('key2'), keys.get('key3'), keys.get('key4'))
+                return True
+
+        return False
+
+    def _post_request(self, callback, key2, key3, key4):
+        try:
+            url = 'https://fpcyweb.tax.sh.gov.cn:1001/WebQuery/query'
+            data = {
+                'callback': callback,
+                'fpdm': str(self.bill_id),
+                'fphm': str(self.bill_num),
+                'kprq': str(self.bill_date),
+                'fpje': str(self.bill_money),
+                'fplx': key4,
+                'yzm': self.verification_code,
+                'yzmSj': key2, #str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+                'index': key3,
+                'iv': self.get_crypto(), #b2a_hex(Random.get_random_bytes(128 / 8)),
+                'salt': self.get_crypto(), #b2a_hex(Random.get_random_bytes(128 / 8)),
+                '_': ''.join([random.choice(string.digits) for _ in range(13)])
+            }
+            print(url)
+            print(json.dumps(data))
+            resp = requests.post(url, data=json.dumps(data), verify=False)
+            status_code = resp.status_code
+            print('*************')
+            print(resp.content, resp.text)
+            print(status_code)
+        except Exception as e:
+            print(u'请求验证失败，正在重试请稍等！- %s' % str(e))
+            return False
+        else:
+            pass
+        return False
+
+    def get_crypto(self):
+        f = open("./crypto-js-develop/src/core.js", 'r')
+        line = f.readline()
+        htmlstr = ''
+        while line:
+            htmlstr = htmlstr + line
+            line = f.readline()
+        ctx = execjs.compile(htmlstr)
+        return ctx.call('test', 16)
 
     def __str__(self):
         return "bill_id:%s bill_num:%s bill_date:%s bill_money:%s " % (
@@ -150,14 +243,6 @@ class Bill(object):
 
 
 class Excel(object):
-    def __init__(self):
-        self.path = '/Users/wangyangyang/Desktop/test.bmp'
-
-    def save(self, text):
-        with open(self.path, 'w') as f:
-            f.write(base64.b64decode(text))
-            # f.write(text)
-
     def setup(self, path):
         if not os.path.isfile(path):
             raise Exception(u'文件%s不存在' % path)
@@ -174,39 +259,35 @@ class Excel(object):
             #         sheet["A3"] != '开票日期' or sheet["A4"] != '开具金额（不含税）':
             #     raise Exception(u'Excel格式错误，请检查')
 
+            # 按列读取
             for i in range(1, sheet.max_column):
                 bills.append(Bill([row.value for row in sheet[chr(65 + i)]]))
+
+            # 按行读取
+            # for i in range(1, sheet.max_row):
+            #     a = [col.value for col in sheet[str(i)]]
+            #     if len(a) == 4:
+            #         bills.append(Bill(a, i))
 
         self.bills = bills
 
     def check(self):
+        if not hasattr(self, 'bills'):
+            print(u'请先执行setup...')
+            return
+
         for bill in self.bills:
-            keys = bill._get_two_dimension_picture()
-            # print(keys)
-            # print(type(keys))
+            is_ok = False
 
-            dmt = DamatuApi("xiaolongchang", "858993460")
-            print('*********')
-            if keys:
-                self.save(keys['key1'])
-                print(dmt.decode(self.path, 60))  # 上传打码
+            while not is_ok:
+                is_ok = bill.check_task()
                 break
+            break
 
 
+dmt = DamatuApi(DamatuUserName, DamatuUserPassWD)
 
-
-# 调用类型实例：
-# 1.实例化类型 参数是打码兔用户账号和密码
 if __name__ == '__main__':
-    # dmt = DamatuApi("xiaolongchang", "858993460")
-    # # 2.调用方法：
-    # print(dmt.getBalance())  # 查询余额
-    # print(dmt.decode('0349.bmp', 4))  # 上传打码
-    # print(
-    # dmt.decodeUrl('http://captcha.qq.com/getimage?aid=549000912&r=0.7257105156128585&uin=3056517021', 200))  # 上传打码
-    # print(dmt.reportError('894657096')) #上报错误
-
     excel = Excel()
-    # 文件路径
-    excel.setup('./发票测试.xlsx')
+    excel.setup(Excel_filepath)
     excel.check()
